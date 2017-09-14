1e85d29fd2fb795c4b0930828ca8d43938c17293
Jordan <bigjman1234567@yahoo.com>
2017-09-14 18:48:42 +0000

    changed some things with enemy cars. started implementing power ups

--- Enemy.js
+++ Enemy.js
@@ -1,23 +1,28 @@
+function enemyAssets() {
+    game.load.image('car1', 'assets/car1.png', 136, 300);
+    game.load.image('car2', 'assets/car2.png', 138, 299);
+    game.load.image('car3', 'assets/car3.png', 136, 300);
+    game.load.image('car4', 'assets/car4.png', 143, 300);
+    game.load.image('car5', 'assets/car5.png', 149, 300);
+    game.load.image('car6', 'assets/car6.png', 149, 300);
+    game.load.image('car7', 'assets/car7.png', 149, 300);
+    game.load.image('car8', 'assets/car8.png', 149, 300);
+    game.load.image('car9', 'assets/car9.png', 149, 300);
+    game.load.image('car10', 'assets/car10.png', 149, 300);
+}
+
 function enemyCreate(x, sprite) {
-    
+
     enemyCars = enemy.create(x, -180, sprite);
     enemyCars.scale.setTo(.6, .6);
-    // enemyCars.body.immovable = true;
+    enemyCars.body.immovable = true;
     enemyCars.body.gravity.y = 20;
     enemyCars.body.velocity.y = 50;
     
 }
 
-function resetCar() {
+// function enemyUpdate() {
     
-    // enemyCar1.kill();
     
-    // if (enemyCar.kill) {
-    //     console.log('car killed');
-    // }
     
-}
-
-function enemyUpdate() {
-    
-}
+// }
